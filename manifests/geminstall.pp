4962cb20d7d56b7a7e7e0c7348dd7970b1d0294f
cvquesty <questy@gmail.com>
2019-06-10 13:10:21 -0400

    adding in gemfile installations to support app

---
+++ manifests/geminstall.pp
@@ -0,0 +1,40 @@
+# Manifest to manage the installation of the provided gems
+class cconnect::geminstall {
+
+  # Install Gems on Linux
+  if $facts['os']['name'] == 'Linux' {
+    # Install the Colorize Gem
+    package { 'colorize':
+      ensure   => 'installed',
+      provider => 'gem',
+      source   => '/var/tmp/nhdata/colorize-0.8.1.gem',
+      require  => Archive['/var/tmp/nhdata/colorize-0.8.1.gem'],
+    }
+
+    # Install the JSON gem
+    package { 'json':
+      ensure   => 'installed',
+      provider => 'gem',
+      source   => '/var/tmp/nhdata/json-2.2.0.gem',
+      require  => Archive['/var/tmp/nhdata/json-2.2.0.gem'],
+    }
+  }
+
+  if $facts['os']['name'] == 'Windows' {
+    # Install the colorize Gem
+    package { 'colorize':
+      ensure   => 'installed',
+      provider => 'gem',
+      source   => 'C:/nhdata/colorize-0.8.1.gem',
+      require  => Archive['C:/nhdata/colorize-0.8.1.gem'],
+    }
+
+    # Install the JSON Gem
+    package { 'json':
+      ensure   => 'installed',
+      provider => 'gem',
+      source   => 'C:/nhdata/json-2.2.0.gem',
+      require  => Archive['C:/nhdata/json-2.2.0.gem'],
+    }
+  }
+}
