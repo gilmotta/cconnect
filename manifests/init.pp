02125a169ffb18963d7f3670acbcef9c18c5dac4
cvquesty <questy@gmail.com>
2019-06-10 13:15:12 -0400

    including all classes and then specifically ordering them

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,14 @@
 # Main Module Entry Point
 class cconnect {
 
+  include cconnect::fileprep
+  include cconnect::vboxprep
+  include cconnect::geminstall
+  include cconnect::vboxinstall
 
+  Class['cconnect::fileprep']
+  ->Class['cconnect::vboxprep']
+  ->Class['cconnect::geminstall']
+  ->Class['cconnect::vboxinstall']
 
 }
